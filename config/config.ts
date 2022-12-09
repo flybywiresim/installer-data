2b29919d1b06aacbbbafc5b48a5cbe68473adae8
Frank Kopp <frank@familie-kopp.de>
2022-12-09 11:59:50 +0100

    Adding new name for LVFR 321 Comp add-on

--- config/config.ts
+++ config/config.ts
@@ -156,6 +156,10 @@ const config: Configuration = {
                             description: "It is recommended to remove this add-on before installing and using the A32NX. This add-on is known to override A32NX components and to break the A32NX.",
                         },
                         {
+                            title: 'LVFR A321neo Extreme',
+                            description: "It is recommended to remove this add-on before installing and using the A32NX. This add-on is known to override A32NX components and to break the A32NX.",
+                        },
+                        {
                             title: '[MOD] Mugz FBW A32NX Dev',
                             creator: "Mugz",
                             description: "It is required to remove this add-on before installing and using the A32NX. This add-on overrides A32NX components and renders the A32NX unusable.",
