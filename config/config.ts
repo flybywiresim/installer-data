1d29e095452159c26aefa826eaaa8e7ed40a304d
Saschl <19493808+Saschl@users.noreply.github.com>
2024-10-08 10:08:05 +0200

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -307,7 +307,7 @@ const config: Configuration = {
                     backgroundImageUrls: ['https://flybywirecdn.com/installer/media-assets/addon-headers/fbw-simbridge/0.png'],
                     backgroundImageShadow: false,
                     shortDescription: 'Airbus A380-800',
-                    description: 'SimBridge is an external application which allows FBW aircraft to communicate with components located outside the simulator. SimBridge will be used for a number of features requiring external data (such as TAWS terrain display), as well as for functionality providing remote access to aircraft systems or data.',
+                    description: `<b><span class="text-utility-amber">&#9888; Note: Starting with version 0.6.0 custom resources like PDF Charts and Company Routes need to be located inside the Documents folder. Please refer to the documentation.</span></b> \n\n SimBridge is an external app that enables FlyByWire Simulations aircraft to communicate outside your simulator. From remote displays to external terrain display rendering, it is used for a variety of optional features.`,
                     targetDirectory: 'flybywire-externaltools-simbridge',
                     tracks: [
                         {
