0f059d419bd15b3840e04a97188213c752e61d04
Frank Kopp <frank@familie-kopp.de>
2024-11-16 13:54:53 +0100

    Update config/config.ts
    Co-authored-by: Florian Scheuner <github@florian-scheuner.de>

--- config/config.ts
+++ config/config.ts
@@ -326,7 +326,7 @@ const config: Configuration = {
                             name: 'Development (8K)',
                             key: 'a380x-dev-8k',
                             url: 'https://flybywirecdn.com/addons/a380x/master-8k',
-                            alternativeUrls: [ ],
+                            alternativeUrls: [],
                             description: 'Development will have the latest features that will end up in the next stable. ' +
                                 'Although every change is QA-tested bugs are a little more likely. It updates whenever something is added to ' +
                                 'the \'master\' branch on Github. Please visit our discord for support. \n\n' +
